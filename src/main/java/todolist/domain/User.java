cea5f3535fa777b799123a93c2702fad33e351a6
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-03-17 15:26:58 -0300

    feat(User): add class

---
+++ src/main/java/todolist/domain/User.java
@@ -0,0 +1,48 @@
+package todolist.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Setter
+  @Column(nullable = false, length = 30)
+  private String username;
+
+  @Setter
+  @Column(nullable = false, length = 320, unique = true)
+  private String email;
+  
+  @Setter
+  @Column(nullable = false)
+  private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+}
