c7ef5cfbf77e4cc8816aadefc5f68514e80fd602
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-03-23 21:05:25 -0300

    feat(UserController): add execute function

--- src/main/java/todolist/interfaces/rest/users/UserController.java
+++ src/main/java/todolist/interfaces/rest/users/UserController.java
@@ -8,15 +8,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import todolist.application.users.CreateUserUseCase;
 import todolist.application.users.GetAllUsersUseCase;
 import todolist.application.users.GetUserByIdUseCase;
-import todolist.interfaces.rest.users.requests.UserCreateRequestDTO;
+import todolist.application.users.UpdateUserByIdUseCase;
+import todolist.interfaces.rest.users.requests.UserRequestDTO;
 import todolist.interfaces.rest.users.responses.UserResponseDTO;
 
 @RequiredArgsConstructor
@@ -26,9 +29,10 @@ public class UserController {
   private final CreateUserUseCase createUserUseCase;
   private final GetAllUsersUseCase getAllUsersUseCase;
   private final GetUserByIdUseCase getUserByIdUseCase;
+  private final UpdateUserByIdUseCase updateUserByIdUseCase;
 
   @PostMapping
-  public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserCreateRequestDTO request) {
+  public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid UserRequestDTO request) {
     UserResponseDTO response = createUserUseCase.execute(request);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -44,6 +48,13 @@ public class UserController {
   @GetMapping("/{id}")
   public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id) {
     UserResponseDTO response = getUserByIdUseCase.execute(id);
+
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<UserResponseDTO> updateUserById(@PathVariable UUID id, UserRequestDTO request) {
+    UserResponseDTO response = updateUserByIdUseCase.execute(id, request);
 
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
