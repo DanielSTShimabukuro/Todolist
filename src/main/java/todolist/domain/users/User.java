51491f2f85c671434a778bc8aae58323d1fe70a0
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-03-26 16:20:43 -0300

    feat(User): add addItem function

--- src/main/java/todolist/domain/users/User.java
+++ src/main/java/todolist/domain/users/User.java
@@ -1,22 +1,28 @@
 package todolist.domain.users;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import todolist.domain.items.Item;
 import todolist.interfaces.rest.users.requests.UserRequestDTO;
 
 @Entity(name = "users")
@@ -47,9 +53,16 @@ public class User {
   @UpdateTimestamp
   private LocalDateTime updatedAt;
 
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Item> items = new HashSet<>();
+
   public User(UserRequestDTO request) {
     this.username = request.username();
     this.email = request.email();
     this.password = request.password();
+  }
+
+  private void addItem(Item item) {
+    this.items.add(item);
   }
 }
