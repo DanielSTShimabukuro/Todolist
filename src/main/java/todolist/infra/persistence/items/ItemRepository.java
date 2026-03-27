485d2c0ed6034b41f2f36de78d4b49ed07f30039
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-03-27 04:42:24 -0300

    feat(ItemRepository): add interface

---
+++ src/main/java/todolist/infra/persistence/items/ItemRepository.java
@@ -0,0 +1,13 @@
+package todolist.infra.persistence.items;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import todolist.domain.items.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<UUID, Item> {
+  
+}
