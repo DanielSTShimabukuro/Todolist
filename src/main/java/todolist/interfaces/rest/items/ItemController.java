6ae4a96432d9c5922dc27ab928f35428f4379684
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-04-01 17:10:01 -0300

    feat(ItemController): add listItemsUseCase function

--- src/main/java/todolist/interfaces/rest/items/ItemController.java
+++ src/main/java/todolist/interfaces/rest/items/ItemController.java
@@ -1,9 +1,11 @@
 package todolist.interfaces.rest.items;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import todolist.application.items.CreateItemUseCase;
+import todolist.application.items.ListItemsUseCase;
 import todolist.interfaces.rest.items.requests.ItemRequestDTO;
 import todolist.interfaces.rest.items.responses.ItemResponseDTO;
 
@@ -21,11 +24,19 @@ import todolist.interfaces.rest.items.responses.ItemResponseDTO;
 @RequestMapping("/v1/users/{userId}/items")
 public class ItemController {
   private final CreateItemUseCase createItemUseCase;
+  private final ListItemsUseCase listItemsUseCase;
 
   @PostMapping
   public ResponseEntity<ItemResponseDTO> createItem(@PathVariable UUID userId, @RequestBody @Valid ItemRequestDTO request) {
     ItemResponseDTO response = this.createItemUseCase.execute(userId, request);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+  
+  @GetMapping
+  public ResponseEntity<List<ItemResponseDTO>> listItemsUseCase() {
+    List<ItemResponseDTO> response = this.listItemsUseCase.execute();
+
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 }
